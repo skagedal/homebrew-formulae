4a5cbec22bca1a82b379b913016b1ac195a25e24
Simon Kågedal Reimer <skagedal@gmail.com>
2019-05-30 19:10:11 +0200

    Add 0.1.0 release of xcode-simulator-tool

--- Formula/xcode-simulator-tool.rb
+++ Formula/xcode-simulator-tool.rb
@@ -1,6 +1,8 @@
 class XcodeSimulatorTool < Formula
   desc "Manage Xcode simulators and their Certificate Authorities"
   homepage "https://github.com/skagedal/xcode-simulator-tool"
+  url "https://github.com/skagedal/xcode-simulator-tool.git",
+      :tag => "0.1.0", :revision => "5fb7d6ebc0fb90d6d2edc95199619e032a33aace"
   head "https://github.com/skagedal/xcode-simulator-tool.git"
 
   depends_on :xcode => ["10.2", :build]
