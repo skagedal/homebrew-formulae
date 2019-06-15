78bab2216dd3d57d85850318f0e42ea101a171c2
Simon Kågedal Reimer <skagedal@gmail.com>
2019-06-15 20:18:07 +0200

    xcode-simulator-cert 0.2.1

--- Formula/xcode-simulator-cert.rb
+++ Formula/xcode-simulator-cert.rb
@@ -2,7 +2,7 @@ class XcodeSimulatorCert < Formula
   desc "Manage Xcode simulators and their Certificate Authorities"
   homepage "https://github.com/skagedal/xcode-simulator-cert"
   url "https://github.com/skagedal/xcode-simulator-cert.git",
-      :tag => "0.2.0", :revision => "9ddde6e92ce40d68907c49f1bfcd9e1cf5397923"
+      :tag => "0.2.1", :revision => "549361c7b29bc4787881d3ece94c745d1a628906"
   head "https://github.com/skagedal/xcode-simulator-cert.git"
 
   depends_on :xcode => ["10.2", :build]
